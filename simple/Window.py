93125c6f052eb169a2723f53b2c9aad8ffc50cc7
assazonov <95968945+assazonov@users.noreply.github.com>
2021-12-11 17:16:45 +0300

    Add files via upload

---
+++ simple/Window.py
@@ -0,0 +1,7 @@
+from PyQt5.QtWidgets import QWidget
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(400,400)
+        self.show()
---
+++ simple/myproga.py
@@ -0,0 +1,24 @@
+from PyQt5.QtWidgets import QApplication, QWidget
+
+from Window import MyWindow
+
+if __name__ == '__main__':
+    app = QApplication([]) 
+    mainWindow = MyWindow()
+    app.exec_()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
