fe35dfb7403563dc661e2ad69dfb5367e4b6dff5
assazonov <95968945+assazonov@users.noreply.github.com>
2021-12-11 17:22:34 +0300

    test commit

--- simple/Window.py
+++ simple/Window.py
@@ -3,5 +3,6 @@ from PyQt5.QtWidgets import QWidget
 class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
+        someVar = 6
         self.resize(400,400)
         self.show()
