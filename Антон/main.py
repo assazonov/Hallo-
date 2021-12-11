b1524446604ba1ecdae275c20a8b019d39b4ba9f
assazonov <95968945+assazonov@users.noreply.github.com>
2021-12-11 17:18:34 +0300

    Delete Антон directory

--- Антон/main.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import  QApplication,QWidget
-
-from Window import MyWindow
-
-if __name__ == '__main__':
-    app = QApplication([]) #создание объекта приложения
-    mainWindow = MyWindow()
-    #mainWindow.show()
-    app.exec_() #ожидание закрытие программы
