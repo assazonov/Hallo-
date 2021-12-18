4d3b6575a8d65200b057230d5701136dc97ecf72
assazonov <95968945+assazonov@users.noreply.github.com>
2021-12-18 17:24:26 +0300

    Delete MainLayout.py

--- MainLayout.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QVBoxLayout
-from PyQt5.QtCore import Qt
-from MyLable import myLabel
-
-class mainLayout(QVBoxLayout):
-    def __init__(self):
-        super().__init__()
-        self.myLable = myLabel
-        self.addWidget(self.myLable)
